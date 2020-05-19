6e79ea7db8074dfb1e4fdbb2de2c93fc7a876890
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2020-05-20 01:04:38 +0530

    Update mybot.py
    #TODO
    * option to filter markdown syntax 
    * `^% ([\s\S]*)` for interactive shell execution

--- mybot.py
+++ mybot.py
@@ -1,6 +1,10 @@
 import re, os, sys
+import subprocess
+import threading
+import asyncio
 import discord
 import inspect
+import textwrap
 
 from typing import NewType, Sequence, Tuple, Mapping, Dict, Pattern, Union, Callable, Optional
 from typing_extensions import Protocol as FnProtocol
@@ -13,14 +17,16 @@ UserLevel = NewType('UserLevel',int) # Admin,Member = 0,n
 Msg = NewType('Msg',discord.Message)
 
 class Fn_with_level(FnProtocol):
-    def __call__(self,*groups:str,msg:Msg,level:Optional[UserLevel]=None)->str: ...
+    def __call__(self,*groups:str,msg:Msg,level:Optional[UserLevel]=None)->Optional[str]: ...
 class Fn_without_level(FnProtocol):
-    def __call__(self,*groups:str,msg:Msg)->str: ...
+    def __call__(self,*groups:str,msg:Msg)->Optional[str]: ...
 ExecFn = Union[Fn_with_level,Fn_without_level]
 
 class Manager:
     """To manage all commands with the author's permission level
     """
+    
+    ContentFieldLimits = 2000
     
     def __init__(self,
                  P2F:Mapping[PatternVstr,ExecFn]={
@@ -55,19 +61,26 @@ class Manager:
         for cmd,Fn in self.P2F.items():
             if type(cmd) is str:
                 if cmd == message.content:
-                    response = self.execute(Fn=Fn,msg=message)
-                    if response:
+                    response = await self.execute(Fn=Fn,msg=message)
+                    if response and len(response) > self.ContentFieldLimits:
+                        for response in textwrap.wrap(response,self.ContentFieldLimits):
+                            await message.channel.send(response)
+                    elif response:
                         await message.channel.send(response)
             else:
                 match = cmd.match(message.content)
                 if match:
-                    response = self.execute(*match.groups(),Fn=Fn,msg=message)
-                    if response:
+                    response = await self.execute(*match.groups(),Fn=Fn,msg=message)
+                    if response and len(response) > self.ContentFieldLimits:
+                        for response in textwrap.wrap(response,self.ContentFieldLimits):
+                            await message.channel.send(response)
+                    elif response:
                         await message.channel.send(response)
     
-    def execute(self,*args:str,Fn:ExecFn,msg:Msg):
+    async def execute(self,*args:str,Fn:ExecFn,msg:Msg)->Optional[str]:
         """To execute the matched function, if the user has requied privileges to execute.
         It gets the required level of each function from the function's kwargument 'level'.
+        Then return which Fn(*args,msg=msg) returns.
         """
         
         if Fn.__kwdefaults__ and 'level' in Fn.__kwdefaults__ and self.users != None:
@@ -85,11 +98,11 @@ class Manager:
                 else:
                     level = None
             if inspect.iscoroutinefunction(Fn):
-                return Fn(*args,msg=msg,level=level)
+                return await Fn(*args,msg=msg,level=level)
             else:
                 return Fn(*args,msg=msg,level=level)
         if inspect.iscoroutinefunction(Fn):
-            return Fn(*args,msg=msg)
+            return await Fn(*args,msg=msg)
         else:
             return Fn(*args,msg=msg)
 
@@ -115,11 +128,13 @@ class Matcher:
         
         self.P2F = P2F if P2F != None else dict()
     
-    def __call__(self,match_with:PatternVstr):
+    def __call__(self,match_with:PatternVstr)->Callable[[ExecFn],ExecFn]:
         """For saving decorated function
         """
+        
         def updateP2F(Fn:ExecFn):
             self.P2F[match_with] = Fn
+            return Fn
         return updateP2F
 
 if __name__ == "__main__":
@@ -127,15 +142,16 @@ if __name__ == "__main__":
     @client.event
     async def on_ready():
         print(f'{client.user.name} is Online :]')
-
+    
     key = Matcher()
+    ### Python shell
     @key(re.compile(r'^!\$ ([\s\S]*)$'))
     def Exec(*args,msg):
         try:
             code = compile(args[0],'code','eval')
             global _
             _ = eval(code,globals(),globals())
-            return _
+            return repr(_)
         except SyntaxError:
             try:
                 code = compile(args[0],'code','exec')
@@ -145,12 +161,24 @@ if __name__ == "__main__":
                 return repr(sys.exc_info())
         except:
             return repr(sys.exc_info())
+    ### System shell
+    processes = list()
     @key(re.compile(r'^\$ ([\s\S]*)$'))
-    def Shell(*args,msg):
-        return os.popen(args[0]).read()
+    async def Shell(*args,msg):
+        process = subprocess.Popen(args[0], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+        processes.append(process)
+        response = list()
+        threading.Thread(target=lambda p,r:r.extend(p.communicate()),args=(process,response)).start()
+        while not response:
+            await asyncio.sleep(.3)
+        processes.remove(process)
+        output, error = response[0].decode(), response[1].decode()
+        outAerr = output.strip(os.linesep) + (os.linesep if error else '') + os.linesep.join([ '?>'+x for x in error.splitlines() ])
+        return '$ '+args[0] + os.linesep + outAerr + os.linesep + f'Return code = {process.returncode}'
+    ### To exit
     @key('!exit')
     def Exit(*args,msg):
         sys.exit(0)
-
+    
     client.event(Manager(key.P2F).on_message)
     client.run(os.environ['DBToken'])
